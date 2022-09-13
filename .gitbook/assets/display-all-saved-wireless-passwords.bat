e2aaecd10545d390695ec6f9ed162f596df7a7c2
Leon Teale <leonteale89@gmail.com>
2022-09-13 11:03:57 +0000

    GitBook: [#245] No subject

---
+++ .gitbook/assets/display-all-saved-wireless-passwords.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal enabledelayedexpansion
+for /f "tokens=2delims=:" %%a in ('netsh wlan show profile ^|findstr ":"') do (
+    set "ssid=%%~a"
+    call :getpwd "%%ssid:~1%%"
+)
+:getpwd
+set "ssid=%*"
+for /f "tokens=2delims=:" %%i in ('netsh wlan show profile name^="%ssid:"=%" key^=clear ^| findstr /C:"Key Content"') do echo ssid: %ssid% pass: %%i
--- wireless-testing/wireless/get-wireless-password-from-windows-cmd.md
+++ wireless-testing/wireless/get-wireless-password-from-windows-cmd.md
@@ -3,3 +3,7 @@
 ```
 Netsh wlan show profile name="Wi-F name" key=clear
 ```
+
+For convenience, here is a batch script that will display all the saved passwords.
+
+{% file src="../../.gitbook/assets/display-all-saved-wireless-passwords.bat" %}
